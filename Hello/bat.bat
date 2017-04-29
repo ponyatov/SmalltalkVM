93ddfe8fe4438bf76ca6075583c47498e4994a54
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-29 22:30:34 +0400

    + X_Module works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ Hello/bat.bat
@@ -0,0 +1,1 @@
+@gvim -c "set encoding=utf8" -p log.log hpp.hpp cpp.cpp Makefile
---
+++ Hello/cpp.cpp
@@ -0,0 +1,7 @@
+#include "hpp.hpp"
+int main(int argc,char *argv[]) {
+	wcout << "программа: " << argv[0] << endl;
+}
+<< argv[0] << endl;
+}
+
