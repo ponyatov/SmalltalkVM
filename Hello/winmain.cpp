f4ef5a33befae558078b4b04341d962a4856a650
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-29 23:49:15 +0400

    + res/winmain samples
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ Hello/winmain.cpp
@@ -0,0 +1,7 @@
+#include "hpp.hpp"
+
+// Our application entry point.
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	cout << "\n\nWinMain\n\n";
+}
